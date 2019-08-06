0a3229fdc067f790918573909b8da286b5eb8af2
Tanner <TannerRLA@gmail.com>
2019-08-06 13:56:26 -0700

    complete application, prepping for production

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,13 @@
 class ChatroomController < ApplicationController
+	before_action :require_user
+
 	def index
-		@messages = Message.all 
+		@message = Message.new
+		@messages = Message.custom_display
 	end
+
+
+
+
 
 end
