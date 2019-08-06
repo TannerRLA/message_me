2a013f326c17c9ce575cb6464ae9169c101d288a
Tanner <TannerRLA@gmail.com>
2019-08-05 18:31:25 -0700

    added iterator to chat room with real time messaging

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Tanner", password:"password")
+User.create(username: "Ashley", password:"password1")
+User.create(username: "Scout", password:"password2")
+User.create(username: "Rookie", password:"password3")
